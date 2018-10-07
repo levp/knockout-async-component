987459f01af60d4d316f09a148231b70da31cad3
levp <levi2p@gmail.com>
2018-10-08 00:18:26 +0300

    Added usage of loaded data to example.

--- web/src/data-widget.comp.js
+++ web/src/data-widget.comp.js
@@ -2,13 +2,16 @@ ko.components.register('data-widget', {
 	loadData() {
 		return new Promise(resolve => {
 			setTimeout(() => {
-				resolve({data: 42});
-			}, 2000);
+				resolve({value: 42});
+			}, 1000);
 		});
 	},
 	viewModel: class {
+		constructor(props) {
+			this.data = props.data;
+		}
 	},
 	template: `
-		<b>WOW!</b>
+		<b>WOW! <span data-bind="text: data.value"></span></b>
 	`
 });
